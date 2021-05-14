1381f9bfc117fed530584abb8ed934e7356e384e
DESKTOP-IMMK8GB\eboerse <lumbardhhoxha@hotmail.com>
2021-05-14 22:53:59 +0200

    Get Message

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class MainTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public void test_Given_WeWantToSeeAWelcomeMessage_When_WeGetTheMessage_Then_theMessageShouldBeExectlyHelloMaintenanceMonitor() {
+        // arrange
+
+        // act
+        String actual = Main.GetMessage();
+
+        // assert
+        Assertions.assertEquals("hello maintenance monitor", actual);
+    }
 }
