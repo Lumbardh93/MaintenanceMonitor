0925080d15fcf78ef5b98b07a05a7287268f6d8b
DESKTOP-IMMK8GB\eboerse <lumbardhhoxha@hotmail.com>
2021-05-15 16:13:33 +0200

    new version

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class MainTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+  @Test
+  public void test_Given_WeWanToSeeAWelcomeMessage_When_WeGetTheMessage_Then_theMessageShouldBeExactlyHelloMaintenanceMonitor() {
+      // arrange
+
+      // act
+      String actual = Main.GetMessage();
+      // assert
+      Assertions.assertEquals("hello maintenance monitor", actual);
+  }
 }
